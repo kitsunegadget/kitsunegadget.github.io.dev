bce439f0842ab585e769d488d3ed6b7f51448ac2
Y_Yu <kitsunegadget@gmail.com>
2019-07-16 21:50:06 +0900

    add: moving literal

--- src/components/Content1PfGrid.vue
+++ src/components/Content1PfGrid.vue
@@ -9,27 +9,27 @@
                 </span>
             </div>
             <div id="line1" class="str_delay">
-                <p>NAME</p>
+                <p class="moving-literal">NAME</p>
                 <hr />
             </div>
             <div id="line2" class="str_delay">
                 <hr />
             </div>
             <div id="nameplate" class="str_delay">
-                <p>Yu\w* (beta)</p>
+                <p class="moving-literal"> Yu\w* (beta)</p>
             </div>
             <div id="line3" class="str_delay">
-                <p>INTEREST</p>
+                <p class="moving-literal">INTEREST</p>
                 <hr />
             </div>
             <div id="line4" class="str_delay">
                 <hr />
             </div>
             <div id="interest" class="str_delay">
-                <p>Video Game</p>
-                <p>Programming</p>
-                <p>Illust</p>
-                <p>Sound</p>
+                <p class="moving-literal">Video Game</p>
+                <p class="moving-literal">Programming</p>
+                <p class="moving-literal">Illust</p>
+                <p class="moving-literal">Sound</p>
                 <p></p>
             </div>
         </div>
@@ -37,6 +37,7 @@
 </template>
 
 <script>
+import { setInterval, setTimeout } from 'timers';
 export default {
     
 }
@@ -47,6 +48,105 @@ export default {
 //     }
 //     // console.log("width changed.");
 // }
+
+var movingLiteral = {
+    run: function()
+    {
+        this.literals = document.getElementsByClassName("moving-literal");
+    
+        var list = [];
+        for(var p of this.literals)
+        {
+            list = list.concat(p.innerText);
+        }
+        this.origLiterals = list;
+
+        document.addEventListener("mouseenter", () => {
+            this.outsideMouse = false;
+            var i = 0;
+            for(var p of this.literals)
+            {
+                p.innerText = this.origLiterals[i];
+                i++;
+            }
+            //console.log("mouseenter", this.origLiterals);
+        });
+        document.addEventListener("mouseleave", () => {
+            setTimeout(() => {
+                this.outsideMouse = true;
+            }, 30000);
+        });
+
+        this.outsideMouse = false;
+        this.changeStart();
+    },
+
+    changeStart: function() 
+    {
+        setInterval(() => {
+            if(this.outsideMouse)
+            {
+                for(var p of this.literals)
+                {
+                    if(this.slotChoose() === 1)
+                    {
+                        var index = this.lineChoose(p.innerText.length);
+                        
+                        p.innerText = p.innerText.replace(p.innerText[index], this.replaceLiteral());
+                        //console.log("changeText", p);
+                    }
+                }
+            }
+        }, 100);
+    },
+
+    slotChoose: function() 
+    {
+        return Math.round(Math.random() * 3);
+    },
+
+    lineChoose: function(length)
+    {
+        return Math.floor(Math.random() * length);
+    },
+
+    replaceLiteral: function()
+    {
+        // 利用 unicode 範囲
+        // \u0021 - \u007E
+        // \u00A1 - \u02AF
+        for(;;)
+        {
+            var code = Math.floor(Math.random() * 0x2AF);
+            if(code >= 0x21 && code <= 0x7E || code >= 0xA1)
+            {
+                break;
+            }
+        }
+        
+        if(code.length < 2)
+        {
+            code = "000" + code;
+        }
+        else if (code.length < 3)
+        {
+            code = "00" + code;
+        }
+        else if (code.length < 4)
+        {
+            code = "0" + code;
+        }
+
+        return String.fromCharCode(code);
+    }
+}
+
+setTimeout(() => {
+    movingLiteral.run(); 
+}, 1000);
+
+
+
 
 </script>
 
