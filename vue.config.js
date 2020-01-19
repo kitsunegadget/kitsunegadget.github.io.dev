d59895f5218462ead1cf1d9228726053a7b97837
Y_Yu <kitsunegadget@gmail.com>
2020-01-20 00:07:17 +0900

    Ignoring test data in public folder when building.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
+const isLegacyBundle = !process.env.VUE_CLI_MODERN_BUILD;
+//console.log(process.env);
 module.exports = {
-  productionSourceMap: false
+  productionSourceMap: false,
+  chainWebpack: config => { 
+    // distへコピーしないpublicファイルの設定（テスト用の画像など）。
+    // 注：モダンモードでのビルド時、レガシーバンドルのoptionsが
+    // 空となり以下がエラーになります。
+    // しかしながらモダンモードはレガシーバンドルを生成した結果から
+    // モダンバンドルを生成しているようなので、設定を反映させるため
+    // レガシービルドでは以下を省くようにします。
+    if (!isLegacyBundle) {
+      console.log("through config.");
+      config.plugin('copy').tap(([options]) => {
+        options[0].ignore.push('product_imgs/**', 'gallery_imgs/**');
+        return [options];
+      });
+    }
+  }
 }
