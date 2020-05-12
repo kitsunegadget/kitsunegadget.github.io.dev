d34dbce8abc8fdb3a59e4dc85ff4222b72f32483
Y_Yu <kitsunegadget@gmail.com>
2020-05-12 16:55:21 +0900

    update bio

---
+++ src/components/BioData.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="bio-data">
+        <!-- <p>{{ description }}</p> -->
+        <h3>Primary Skills (intermediate?)</h3>
+        <ul>
+            <li v-for="text in primarySkills" :key="text">
+                {{ text }}
+            </li>
+        </ul>
+        <h3>Secondary Skills (beginner to middle)</h3>
+        <ul>
+            <li v-for="text in secondarySkills" :key="text">
+                {{ text }}
+            </li>
+        </ul>
+        <h3>Other Skills (interesting)</h3>
+        <ul>
+            <li v-for="text in otherSkills" :key="text">
+                {{ text }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    data() {
+        return {
+            description: "先の見えない場所を目指して…",
+            primarySkills: [
+                "html, css, javascript (Typescript), Python, C#",
+                "Vue, Nuxtjs, Unity",
+                "FE (基本情報技術者)"
+            ],
+            secondarySkills: [
+                "C, C++, JAVA, Shader programming",
+                "React, Xamarin",
+            ],
+            otherSkills: [
+                "Guitar, Base guitar, "
+            ]
+        }
+    }
+})
+</script>
+
+<style lang="scss">
+.bio-data {
+    color: $white-e;
+    width: 100%;
+    margin: auto;
+
+    h3 {
+        padding: 5px;
+        background: #0006;
+        color: $orange;
+
+        &::before {
+            content: "‖";
+            margin-right: 10px;
+        }
+    }
+}
+</style>
--- src/views/Bio.vue
+++ src/views/Bio.vue
@@ -1,17 +1,30 @@
 <template>
     <div>
-        <BioGrid />
+        <div id="dock">
+            <BioGrid />
+            <BioData />
+        </div>
         <Social />
     </div>
 </template>
 <script>
 import BioGrid from '@/components/BioGrid.vue';
+import BioData from '@/components/BioData.vue';
 import Social from '@/components/Social.vue';
 
 export default {
     components: {
         BioGrid,
+        BioData,
         Social
     }
 }
 </script>
+
+<style lang="scss">
+#dock{
+    margin:auto;
+    /* padding-bottom: 10px;  */
+    max-width: 700px;
+}
+</style>
